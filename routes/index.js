6bceb2a8ca5dd1482b78f1a4728d14f01681588a
Sergio Vicente de las Heras <sergio.v.he@gmail.com>
2018-03-06 22:47:48 +0100

    Minor refactor

--- routes/index.js
+++ routes/index.js
@@ -3,55 +3,58 @@ var request = require('request');
 var router = express.Router();
 
 router.get('/status', (req, res) =>
-  res.json({
-    status: "ok"
-  })
+	res.json({
+		status: "ok"
+	})
 );
 
 router.post('/pullrequest', (req, res, next) => {
 
-  var titile = req.body.resource.title
-  var autor = req.body.resource.createdBy.displayName
-  var link = req.body.resource._links.web.href
-  var creation_date = req.body.resource.creationDate
-  var repository = req.body.resource.repository.name
+	var titile = req.body.resource.title
+	var autor = req.body.resource.createdBy.displayName
+	var link = req.body.resource._links.web.href
+	var creation_date = req.body.resource.creationDate
+	var repository = req.body.resource.repository.name
 
-  request.post({
-    headers: {
-      'content-type': 'application/json'
-    },
-    url: process.env.WEBHOOKS_TEAMS_URL,
-    body: `
-      {
-        "@type": "MessageCard",
-        "@context": "http://schema.org/extensions",
-        "themeColor": "0076D7",
-        "summary": "${autor} created a new pull request",
-        "sections": [{
-            "activityTitle": "${autor} created a new pull request",
-            "activitySubtitle": "${titile}",
-            "facts": [{
-                "name": "Repository",
-                "value": "${repository}"
-            }, {
-                "name": "Creation date",
-                "value": "${creation_date}"
-            }],
-            "markdown": true
-        }],
-        "potentialAction": [{
-          "@type": "OpenUri",
-          "name": "Review",
-          "targets": [
-            { "os": "default", "uri": "${link}" }
-          ]
-        }]
-    }`
-  }, function (err, response, body) {
-    res.json({
-      status: "success"
-    });
-  });
+	request.post({
+		headers: {
+			'content-type': 'application/json'
+		},
+		url: process.env.WEBHOOKS_TEAMS_URL,
+		body: `{
+			"@type": "MessageCard",
+			"@context": "http://schema.org/extensions",
+			"themeColor": "0076D7",
+			"summary": "${autor} created a new pull request",
+			"sections": [{
+				"activityTitle": "${autor} created a new pull request",
+				"activitySubtitle": "${titile}",
+				"facts": [{
+					"name": "Repository",
+					"value": "${repository}"
+				}, {
+					"name": "Creation date",
+					"value": "${creation_date}"
+				}],
+				"markdown": true
+			}],
+			"potentialAction": [{
+			"@type": "OpenUri",
+			"name": "Review",
+			"targets": [
+				{ "os": "default", "uri": "${link}" }
+			]
+			}]
+		}`
+	}, (error, response, body) => {
+		if (error) {
+			next(error)
+		}
+
+		res.json({
+			status: 'success'
+		});
+	});
 });
 
 module.exports = router;
