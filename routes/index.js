dcd1a8188ff6bac55143557f09f5bf14a68a8b55
Sergio Vicente de las Heras <sergio.v.he@gmail.com>
2018-06-26 16:26:12 +0200

    Back to the good old card model

--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,8 @@ router.post('/pullrequest', (req, res, next) => {
 
     let titile = req.body.resource.title
     let autor = req.body.resource.createdBy.displayName
-    let autor_id = req.body.resource.createdBy.uniqueName
-    let autor_image = req.body.resource.createdBy.imageUrl
     let link = req.body.resource._links.web.href
     let creation_date = req.body.resource.creationDate
-    let description = req.body.resource.description
-    let project = req.body.resource.repository.project.name
     let repository = req.body.resource.repository.name
 
     request.post({
@@ -27,103 +23,31 @@ router.post('/pullrequest', (req, res, next) => {
             'content-type': 'application/json'
         },
         url: webhooks_teams_url,
-        body: `
-        {
-            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-            "type": "AdaptiveCard",
-            "version": "1.0",
-            "body": [
-                {
-                    "type": "Container",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "Pull request",
-                            "weight": "bolder",
-                            "size": "medium"
-                        },
-                        {
-                            "type": "ColumnSet",
-                            "columns": [
-                                {
-                                    "type": "Column",
-                                    "width": "auto",
-                                    "items": [
-                                        {
-                                            "type": "Image",
-                                            "url": "${autor_image}",
-                                            "size": "small",
-                                            "style": "person"
-                                        }
-                                    ]
-                                },
-                                {
-                                    "type": "Column",
-                                    "width": "stretch",
-                                    "items": [
-                                        {
-                                            "type": "TextBlock",
-                                            "text": "${autor}",
-                                            "weight": "bolder",
-                                            "wrap": true
-                                        },
-                                        {
-                                            "type": "TextBlock",
-                                            "spacing": "none",
-                                            "text": "${autor_id}",
-                                            "isSubtle": true,
-                                            "wrap": true
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    "type": "Container",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "${titile}",
-                            "wrap": true
-                        },
-                        {
-                            "type": "FactSet",
-                            "facts": [
-                                {
-                                    "title": "Repository:",
-                                    "value": "${repository}"
-                                },
-                                {
-                                    "title": "Project:",
-                                    "value": "${project}"
-                                },
-                                {
-                                    "title": "Description:",
-                                    "value": "${description}"
-                                },
-                                {
-                                    "title": "Creation date:",
-                                    "value": "${creation_date}"
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    "type": "ActionSet",
-                    "actions": [
-                        {
-                            "type": "Action.OpenUrl",
-                            "title": "Review",
-                            "url": "${link}"
-                        }
-                    ]
-                }
-            ]
-        }
-		`
+        body: `{
+			"@type": "MessageCard",
+			"@context": "http://schema.org/extensions",
+			"themeColor": "0076D7",
+			"summary": "${autor} created a new pull request",
+			"sections": [{
+				"activityTitle": "${autor} created a new pull request",
+				"activitySubtitle": "${titile}",
+				"facts": [{
+					"name": "Repository",
+					"value": "${repository}"
+				}, {
+					"name": "Creation date",
+					"value": "${creation_date}"
+				}],
+				"markdown": true
+			}],
+			"potentialAction": [{
+			"@type": "OpenUri",
+			"name": "Review",
+			"targets": [
+				{ "os": "default", "uri": "${link}" }
+			]
+			}]
+		}`
     }, (error, response, body) => {
         if (error) {
             console.log(error)
